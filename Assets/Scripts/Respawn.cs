efc4ff9be75387d2aa42a5385b185b0d1676101e
Echo <167356448+EchoDidAThing1048@users.noreply.github.com>
2025-01-21 17:25:56 -0500

    Added Folders and Added Spawn point script

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    Transform player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        foreach(Transform trans in FindObjectsOfType<Transform>() )
+        {
+            if(trans.CompareTag("Player"))
+            {
+                player = trans; //lol
+            }
+        }
+
+        if(player)
+        {
+            player.position = transform.position;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
