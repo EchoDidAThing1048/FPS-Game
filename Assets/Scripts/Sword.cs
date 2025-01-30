72eee584b38c0441a50f46926bcb61e29e08af0b
Echo <167356448+EchoDidAThing1048@users.noreply.github.com>
2025-01-30 17:33:32 -0500

    did shit (IDK LOL)

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 public class Sword : MonoBehaviour
 {
     Animator ani;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ani = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            //Attack
+            ani.SetBool("isAttacking",true);
         }
+        
+    }
+
+    public void StopAttacking()
+    {
+        ani.SetBool("isAttacking", false);
     }
 }
